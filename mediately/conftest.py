e6de6a2d0590bfe71c91ac25e1e05fa2712e8ff6
Levan Begashvili <iamyourfriendforever127>
2020-07-01 20:25:21 +0400

    boilerplate; django cookiecutter

---
+++ mediately/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from mediately.users.models import User
+from mediately.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
